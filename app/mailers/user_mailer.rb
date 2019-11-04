58d07f4ac83fd776d9c52261e4b37bcdb4005123
ArthurWilbrod <comptwil@gmail.com>
2019-11-04 15:05:34 +0100

    new

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,20 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
+
+  def new_participant_email(attendance)
+
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    user_id = attendance.user_id
+    event_id = attendance.event_id
+    @new_participant = User.find(user_id)
+    @organisator = User.find(Event.find(event_id).user_id)
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login'
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @organisator.email, subject: 'Un nouveau participant !')
+
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -34,4 +34,6 @@ class Event < ApplicationRecord
     return true
   end
 
+
+
 end
