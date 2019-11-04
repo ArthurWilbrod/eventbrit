bb8dfca241e66effe2f2dae7c9c3cc3412401af8
ArthurWilbrod <comptwil@gmail.com>
2019-11-04 16:47:26 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'events#index'
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new]
+  resources :registrations, only: [:new]
+  resources :sessions, only: [:new, :destroy]
+  resources :users, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
