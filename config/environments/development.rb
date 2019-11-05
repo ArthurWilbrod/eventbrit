0e7a67c539e256b6a939ae97c931a545e845e03d
ArthurWilbrod <comptwil@gmail.com>
2019-11-05 11:20:25 +0100

    commit devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,5 @@ Rails.application.configure do
   #config.action_mailer.delivery_method = :letter_opener
   #permet d'éteindre (en la passant à false) tout envoi d'email
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'events#index'
 
   resources :events, only: [:index, :new]
-  resources :registrations, only: [:new]
-  resources :sessions, only: [:new, :destroy]
-  resources :users, only: [:show]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
